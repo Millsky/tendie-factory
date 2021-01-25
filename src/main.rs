d4a991c8a6654ff66a33c84303d6ce6b486d7370
Kyle Mills <kyle83567@gmail.com>
2021-01-25 10:38:20 -0800

    WIP: (Add NASDAQ Ticker Frequency)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-fn get_tickers() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+
+#[derive(Serialize, Deserialize)]
+struct RedditContainer<T> {
+    kind: String,
+    data: T,
+}
+
+#[derive(Serialize, Deserialize)]
+struct RedditPost {
+    title: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct RedditListing {
+    children: Vec<RedditContainer<RedditPost>>
+}
+
+fn get_tickers_nasdaq() -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let mut rdr = csv::Reader::from_path("./src/companylist.csv")?;
     let mut tickers: Vec<String> = vec![];
     for result in rdr.records() {
@@ -19,11 +38,28 @@ async fn get_wsb_top() -> Result<String, Box<dyn std::error::Error>> {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 1. Pull all the stock tickers and convert to a vec
-    let tickers = get_tickers()?;
+    let tickers = get_tickers_nasdaq()?;
     // 2. Download and parse the reddit WSB data
     let posts = get_wsb_top().await?;
-    let v: serde_json::Value = serde_json::from_str(&posts)?;
-    println!("{}", v["data"]["children"][0]["data"]["title"]);
+    let v: RedditContainer<RedditListing> = serde_json::from_str(&posts)?;
+    // 3. Calculate the number of occurrences of each ticker in each title
+    let m: Vec<HashSet<&String>> = v.data.children.into_iter().map(|x| {
+        x.data.title
+    }).map(| x | {
+        let mut tickers_in_title = HashSet::new();
+        for ticker in &tickers {
+            match twoway::find_str(&x, &format!(" {} ", ticker)) {
+                None => {
+
+                }
+                Some(t) => {
+                    tickers_in_title.insert(ticker);
+                }
+            }
+        }
+        return tickers_in_title;
+    }).collect();
+    println!("{:?}", m);
     // 3. Determine the weight of each of the posts talking about a given ticker
     // 4. Construct a portfolio fo stocks based on this initial weighting
     Ok(())
