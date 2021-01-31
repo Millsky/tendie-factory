0cef728149cb4c07b4f75e43618662b1ce99e752
Kyle Mills <kyle83567@gmail.com>
2021-01-31 11:54:06 -0800

    Add in additional meta-data from csv file

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
 
 mod nlp;
 
@@ -20,6 +22,38 @@ struct RedditListing {
     children: Vec<RedditContainer<RedditPost>>
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct TickerData {
+    #[serde(rename(deserialize = "Symbol"))]
+    symbol: String,
+    #[serde(rename(deserialize = "Name"))]
+    company_name: String,
+    #[serde(rename(deserialize = "Last Sale"))]
+    last_sale: String,
+    #[serde(rename(deserialize = "Net Change"))]
+    net_change: String,
+    #[serde(rename(deserialize = "% Change"))]
+    net_percent_change: String,
+    #[serde(rename(deserialize = "Market Cap"))]
+    market_capitalization: String,
+    #[serde(rename(deserialize = "Country"))]
+    country: String,
+    #[serde(rename(deserialize = "IPO Year"))]
+    ipo_year: String,
+    #[serde(rename(deserialize = "Volume"))]
+    volume: String,
+    #[serde(rename(deserialize = "Sector"))]
+    sector: String,
+    #[serde(rename(deserialize = "Industry"))]
+    industry: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct PortfolioItem {
+    meta: TickerData,
+    portfolio_weight: f64,
+}
+
 fn get_tickers_nasdaq() -> Result<Vec<String>, Box<dyn std::error::Error>> {
     let mut rdr = csv::Reader::from_path("./src/nasdaq_screener.csv")?;
     let mut tickers: Vec<String> = vec![];
@@ -28,6 +62,18 @@ fn get_tickers_nasdaq() -> Result<Vec<String>, Box<dyn std::error::Error>> {
         tickers.push(String::from(&record[0]));
     }
     Ok(tickers)
+}
+
+fn pull_ticker_meta_data(tickers: Vec<String>) -> Result<HashMap<String, TickerData>, Box<dyn std::error::Error>> {
+    let mut rdr = csv::Reader::from_path("./src/nasdaq_screener.csv")?;
+    let mut ticker_data: HashMap<String, TickerData> = HashMap::new();
+    for result in rdr.deserialize() {
+        let record: TickerData = result?;
+        if tickers.contains(&record.symbol) {
+            ticker_data.insert(String::from(&record.symbol), record);
+        }
+    }
+    Ok(ticker_data)
 }
 
 async fn get_wsb_top() -> Result<String, Box<dyn std::error::Error>> {
@@ -87,6 +133,17 @@ fn calculate_portfolio_weights_simple(ticker_metrics: HashMap<String, i32>) -> H
     portfolio_weights
 }
 
+fn create_portfolio(ticker_metrics: HashMap<String, f64>) -> Vec<PortfolioItem> {
+    let ticker_meta_data = pull_ticker_meta_data( ticker_metrics.keys().map(String::from).collect()).unwrap();
+    ticker_meta_data.into_iter().map(| (key, meta) | {
+        let pi = PortfolioItem {
+            meta,
+            portfolio_weight: ticker_metrics.get(key.as_str()).unwrap().clone(),
+        };
+        return pi;
+    }).collect()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 1. Pull all the stock tickers and convert to a vec
@@ -100,6 +157,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 3. Determine the weight of each of the posts talking about a given ticker
     let portfolio_weights = calculate_portfolio_weights_simple(ticker_metrics);
     // 4. Construct a portfolio of stocks based on this initial weighting
-    println!("{:?}", portfolio_weights);
+    let portfolio = create_portfolio(portfolio_weights);
+    let json_portfolio = serde_json::to_string(&portfolio)?;
+    let mut file = File::create("portfolio.json")?;
+    file.write_all(json_portfolio.as_bytes())?;
     Ok(())
 }
